d5669ab380c7f7c9fac64e01cc8b0d8189d196f9
GrosserBruder <falenangellll@yandex.ru>
2023-02-24 09:18:24 +0700

    10 и 11

--- src/coffees/coffees.controller.ts
+++ src/coffees/coffees.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Get, Param, Post } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Patch, Post } from '@nestjs/common';
 
 @Controller('coffees')
 export class CoffeesController {
@@ -13,7 +13,18 @@ export class CoffeesController {
   }
 
   @Post()
+  // @HttpCode(HttpStatus.GONE) // такой статус код будет возвращаться при ответе без ошибок
   create(@Body() body) {
     return body;
+  }
+
+  @Patch(':id')
+  update(@Param('id') id: string, @Body() body) {
+    return `Обновление кофе #${id}`;
+  }
+
+  @Delete(':id')
+  remove(@Param('id') id: string) {
+    return `Удаление кофе #${id}`;
   }
 }
