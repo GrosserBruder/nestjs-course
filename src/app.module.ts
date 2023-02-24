5bcf9dfb5914e17b14b11a77e486d98021b7036c
GrosserBruder <falenangellll@yandex.ru>
2023-02-24 12:03:45 +0700

    add typeorm

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,22 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { CoffeesModule } from './coffees/coffees.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [CoffeesModule],
+  imports: [
+    CoffeesModule,
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: '192.168.0.49',
+      port: 5432,
+      username: 'postgres',
+      password: 'postgres',
+      database: 'nest-js-course',
+      autoLoadEntities: true,
+      synchronize: true,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
