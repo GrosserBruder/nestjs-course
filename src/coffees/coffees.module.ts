5f1b483c2af806f82d0701df5f5a361058a7d9a2
GrosserBruder <falenangellll@yandex.ru>
2023-02-28 07:44:45 +0700

    37 nonclassbased provider token

--- src/coffees/coffees.module.ts
+++ src/coffees/coffees.module.ts
@@ -5,14 +5,13 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { Coffee } from './entities/coffee.entity';
 import { Flavor } from './entities/flavor.entity';
 import { Event } from '../events/entities/event.entity';
-
-class MockCoffeesService { }
+import { COFFEE_BRANDS } from './coffee.constants';
 
 @Module({
   imports: [TypeOrmModule.forFeature([Coffee, Flavor, Event])],
   controllers: [CoffeesController],
   // providers: [CoffeesService],
-  providers: [{ provide: CoffeesService, useValue: new MockCoffeesService() }],
+  providers: [{ provide: COFFEE_BRANDS, useValue: ['buddy brew', 'nescafe'] }],
   exports: [CoffeesService],
 })
 export class CoffeesModule { }
