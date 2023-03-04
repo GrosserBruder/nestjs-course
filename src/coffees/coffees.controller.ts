bbb5539746278109bfa64b8c61dd004d39f1d6e0
GrosserBruder <falenangellll@yandex.ru>
2023-03-04 14:09:59 +0700

    60 create custom param decorators

--- src/coffees/coffees.controller.ts
+++ src/coffees/coffees.controller.ts
@@ -18,6 +18,7 @@ import { UpdateCoffeeDto } from './dto/update-coffee.dto/update-coffee.dto';
 import { PaginationQueryDto } from 'src/common/dto/pagination-query.dto/pagination-query.dto';
 import { Public } from 'src/common/decorators/public.decorator';
 import { ParseIntPipe } from 'src/common/pipes/parse-int/parse-int.pipe';
+import { Protocol } from 'src/common/decorators/protocol.decorator';
 
 @UsePipes(ValidationPipe) // предпочтительнее
 @Controller('coffees')
@@ -32,8 +33,12 @@ export class CoffeesController {
   @Public()
   @UsePipes(ValidationPipe)
   @Get()
-  async findAll(@Query() paginationQuery: PaginationQueryDto) {
-    await new Promise(resolve => setTimeout(resolve, 5000))
+  async findAll(
+    @Protocol('https') protocol: string,
+    @Query() paginationQuery: PaginationQueryDto
+  ) {
+    console.log(protocol)
+    // await new Promise(resolve => setTimeout(resolve, 5000))
     return this.coffeeService.findAll(paginationQuery);
   }
 
